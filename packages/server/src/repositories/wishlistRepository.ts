e3006a5ef958ec3610e181cee9072c9e1e7d7e7c
Martifas <martynas.grumadas@gmail.com>
2024-12-30 11:30:11 +0200

    implemented wishlist remove

--- packages/server/src/repositories/wishlistRepository.ts
+++ packages/server/src/repositories/wishlistRepository.ts
@@ -1,6 +1,9 @@
 import type { Database, Wishlist } from '@server/database'
 import { wishlistKeys, type WishlistForMember } from '@server/entities/wishlist'
-import type { WishlistRowSelect, WishlistRowUpdate } from '@server/types/wishlist'
+import type {
+  WishlistRowSelect,
+  WishlistRowUpdate,
+} from '@server/types/wishlist'
 import type { Insertable } from 'kysely'
 
 export function wishlistRepository(db: Database) {
@@ -25,16 +28,26 @@ export function wishlistRepository(db: Database) {
         .returning(wishlistKeys)
         .executeTakeFirstOrThrow()
     },
-    async update(id: number, updates: WishlistRowUpdate): Promise<WishlistForMember> {
-          return db
-            .updateTable('wishlist')
-            .set({
-              ...updates,
-              updatedAt: new Date(),
-            })
-            .where('id', '=', id)
-            .returning(wishlistKeys)
-            .executeTakeFirstOrThrow()
-        },
+    async update(
+      id: number,
+      updates: WishlistRowUpdate
+    ): Promise<WishlistForMember> {
+      return db
+        .updateTable('wishlist')
+        .set({
+          ...updates,
+          updatedAt: new Date(),
+        })
+        .where('id', '=', id)
+        .returning(wishlistKeys)
+        .executeTakeFirstOrThrow()
+    },
+    async remove(id: number): Promise<WishlistForMember> {
+      return db
+        .deleteFrom('wishlist')
+        .where('id', '=', id)
+        .returning(wishlistKeys)
+        .executeTakeFirstOrThrow()
+    },
   }
 }
